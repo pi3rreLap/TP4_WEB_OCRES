986e45974426aa9ff82c1c57d858026a934a9427
pierrelapalut <pierre.lapalut@edu.ece.fr>
2021-10-22 11:24:41 +0200

    base (compile pas)

---
+++ tp_4/src/Central.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Banner from 'Banner'
+import Container from 'Container'
+import Footer from 'footer'
+
+export default class Central extends React.Component {
+
+    render() {
+        return (
+            <>
+                <Banner>
+                </Banner>
+
+                <Container>
+                </Container>
+
+                <Footer>
+                </Footer>
+            </>
+        )
+    }
+}
